22285813bba28914ceda3c5c7f9647e51ab93fb0
DollyChen_CYC <dolly726033@gmail.com>
2022-01-24 23:59:59 +0800

    feat: add userSchema and update restaurantSeeder

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,22 +1,32 @@
 const Restaurant = require('../restaurant')
+const User = require('../user')
 const restaurantList = require('../../restaurant_list.json')
-const restaurants = restaurantList.results
-
 const db = require('../../config/mongoose')
 
+const seedRestaurants = restaurantList.results.map(restaurant => {
+  return {
+    name: restaurant.name,
+    name_en: restaurant.name_en,
+    category: restaurant.category,
+    image: restaurant.image,
+    location: restaurant.location,
+    phone: restaurant.phone,
+    google_map: restaurant.google_map,
+    rating: restaurant.rating,
+    description: restaurant.description
+  }
+})
+
 db.once('open', () => {
-  restaurants.forEach( restaurant => {
-    Restaurant.create({
-      name: restaurant.name,
-      name_en: restaurant.name_en,
-      category: restaurant.category,
-      image: restaurant.image,
-      location: restaurant.location,
-      phone: restaurant.phone,
-      google_map: restaurant.google_map,
-      rating: restaurant.rating,
-      description: restaurant.description
-    })
-  })
-  console.log('done')
+  Promise.all(
+    [
+      User.create({
+        email: 'root@example.com',
+        password: '12345678'
+      }),
+      Restaurant.insertMany(seedRestaurants)
+    ]
+  )
+    .then(() => console.log('done'))
+    .catch(error => console.log(error))
 })
