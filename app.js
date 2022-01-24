b4ec1bc0dfef3fed3f01dd74e3ce92f074e26863
DollyChen_CYC <dolly726033@gmail.com>
2022-01-24 19:34:23 +0800

    feat: add method-override and add edit feature

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
 const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/restaurant-list')
 const app = express()
@@ -18,7 +19,7 @@ db.once('open', () => {
 })
 
 // setting template engine
-app.engine('handlebars', exphbs({ defaultLayout: 'main'}))
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
 // setting static files
@@ -26,6 +27,9 @@ app.use(express.static('public'))
 
 // setting body-parser
 app.use(express.urlencoded({ extended: true }))
+
+// use method-override
+app.use(methodOverride('_method'))
 
 // routes setting
 app.get('/', (req, res) => {
@@ -39,9 +43,16 @@ app.get('/restaurants/:id/detail', (req, res) => {
   const id = req.params.id
   return RestaurantList.findById(id)
     .lean()
-    .then(restaurant => res.render('show', {restaurant}))
+    .then(restaurant => res.render('show', { restaurant }))
   // const selectedRestaurant = restaurantList.results.find(restaurant => restaurant._id.toString() === req.params.id)
   // res.render('show', {restaurant: selectedRestaurant})
+})
+
+app.get('/restaurants/:id/edit', (req, res) => {
+  const id = req.params.id
+  return RestaurantList.findById(id)
+    .lean()
+    .then(restaurant => res.render('edit', { restaurant }))
 })
 
 app.get('/search', (req, res) => {
@@ -52,9 +63,9 @@ app.get('/search', (req, res) => {
 
   // compare search keywords
   for (restaurant of restaurantList.results) {
-    const {name, name_en, category, location} = restaurant  // 解構賦值
+    const { name, name_en, category, location } = restaurant  // 解構賦值
     const infoArr = [name, name_en, category, location]
-    
+
     for (word of keywordArr) {
       if (infoArr.some(info => info.toLowerCase().includes(word)) && !searchResults.includes(restaurant)) {
         searchResults.push(restaurant)
@@ -64,9 +75,27 @@ app.get('/search', (req, res) => {
   // notify result not found
   if (searchResults.length === 0) {
     showErrorMsg = true
-  } 
+  }
   // render page
-  res.render('index', {restaurants: searchResults, keyword, showErrorMsg})
+  res.render('index', { restaurants: searchResults, keyword, showErrorMsg })
+})
+
+app.put('/restaurants/:id', (req, res) => {
+  const _id = req.params.id
+  const { name, name_en, category, image, location, phone, description } = req.body
+  RestaurantList.findById(_id)
+    .then(restaurant => {
+      restaurant.name = name
+      restaurant.name_en = name_en
+      restaurant.category = category
+      restaurant.image = image
+      restaurant.location = location
+      restaurant.phone = phone
+      restaurant.description = description
+      return restaurant.save()
+    })
+    .then(() => res.redirect(`/restaurants/${_id}/detail`))
+    .catch(error => console.log(error))
 })
 
 // start and listen on the Express server
